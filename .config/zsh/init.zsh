0283e34eaa268318c01665765d9b604759949314
bepyan <bepyan@naver.com>
2026-03-05 21:51:27 +0900

    refactor: zsh config for speed

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -1,12 +1,12 @@
 source $HOME/.config/zsh/style.zsh
 
-## plugins
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 ## alias
 alias pn=pnpm
 
 ## PATH
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
+## plugins
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- .config/zsh/style.zsh
+++ .config/zsh/style.zsh
@@ -1,8 +1,42 @@
 autoload -Uz colors && colors
 autoload -Uz vcs_info
 
+# vcs
+autoload -Uz vcs_info
+autoload -Uz add-zsh-hook
+
+# git 정보 포맷
+zstyle ':vcs_info:git:*' formats 'git:%b'
+zstyle ':vcs_info:git:*' actionformats 'git:%b*'
+
+# ---- vcs_info 최적화 ----
+
+# 첫 프롬프트에서는 skip (터미널 체감 속도 개선)
+typeset -g _VCS_SKIP_ONCE=1
+
+_vcs_precmd() {
+  if (( _VCS_SKIP_ONCE )); then
+    _VCS_SKIP_ONCE=0
+    return
+  fi
+  vcs_info
+}
+
+# 디렉토리 변경 시에도 갱신
+_vcs_chpwd() {
+  vcs_info
+}
+
+add-zsh-hook precmd _vcs_precmd
+add-zsh-hook chpwd _vcs_chpwd
+
+# 현재 디렉토리가 git repo면 한 번만 미리 실행
+vcs_info
+
 # PROMPT에서 ${...} 변수 확장 허용
 setopt PROMPT_SUBST
+
+# ---- prompt ----
 
 # root면 red, 아니면 yellow
 if [[ $UID -eq 0 ]]; then
@@ -10,13 +44,6 @@ if [[ $UID -eq 0 ]]; then
 else
   NCOLOR="yellow"
 fi
-
-# git 정보 포맷
-zstyle ':vcs_info:git:*' formats 'git:%b'
-zstyle ':vcs_info:git:*' actionformats 'git:%b*'
-
-# 매 프롬프트 직전에 vcs_info 갱신
-precmd() { vcs_info }
 
 # 프롬프트
 PROMPT='%F{'$NCOLOR'}%c ➤ %f'
