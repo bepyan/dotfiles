962b97b85ad77319329d6883be0327681dc24891
bepyan <bepyan@naver.com>
2026-03-16 15:20:52 +0900

    feat: finder preferred List view & style: cleanup finder config

--- .macos.sh
+++ .macos.sh
@@ -40,32 +40,30 @@ done 2>/dev/null &
 
 echo -e "${PURPLE}---- Configuring Finder settings...${NC}"
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+# allow quitting via ⌘ + Q; doing so will also hide desktop icons
 defaults write com.apple.finder QuitMenuItem -bool true
 
-# Finder: show all filename extensions
+# show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Finder: show hidden files by default
+# show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Finder: show status bar
+# show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
-
-# Finder: show path bar
+# show path bar
 defaults write com.apple.finder ShowPathbar -bool true
-
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Keep folders on top when sorting by name
+# Finder: Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
+# Finder: Preferred List view
+defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Disable Finder animations for speed
 defaults write com.apple.finder DisableAllAnimations -bool true
-
-# When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Enable spring loading for directories
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
